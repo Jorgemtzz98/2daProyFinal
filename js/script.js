809bc354940a77c1c0e830730ba409ef90d7c70c
Jorge <jlm.martinez098@gmail.com>
2022-08-23 23:52:01 -0300

    ProyFin2daEntrega

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const idForm = document.getElementById("idForm")
+const inputName =  document.getElementById("inputName")
+const btnSub = document.getElementById("btnSub")
+
+inputName.addEventListener("input",  () => {
+    console.log(nomb_ap.value)
+})
+
+
+inputName.addEventListener("change",  () => {
+    console.log(nomb_ap.value)
+})
+
+
+btnSub.addEventListener("click", () => {
+    alert("Empleado registrado")
+    
+    
+
+})
